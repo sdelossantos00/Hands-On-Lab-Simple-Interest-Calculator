8fca57f13f2d8739e35ab5ff873bb3ec347e96bd
sdelossantos00 <50437374+sdelossantos00@users.noreply.github.com>
2023-02-22 08:27:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-      
+
+ function updateRate()
+ {
+     var rateval = document.getElementById('rate').value;
+     document.getElementById("rate_val").innerText = rateval;
+     
+ }
