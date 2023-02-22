ee6b281ca9bc7d62bcaf6d88387636a2a6525feb
sdelossantos00 <50437374+sdelossantos00@users.noreply.github.com>
2023-02-22 10:23:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principle = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principle * years * rate / 100;
+    var amount = parseInt(principle) + parseFloat(interest);
+    var result = document.getElementById("result");
+    var year = new Date().getFullYear() + parseInt(years);
+    if (principal <= 0) {
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus();
+    } else {
+        result.innerHTML = "If you deposit $" + "<mark>" + principle + "</mark>" + ",\<br\>at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\>You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\>in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    }
 }
 
  function updateRate()
  {
      var rateval = document.getElementById('rate').value;
      document.getElementById("rate_val").innerText = rateval;
-     
+
  }
