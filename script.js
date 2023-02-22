b2dbca0c45a036875c036924e80a2f899de1879f
sdelossantos00 <50437374+sdelossantos00@users.noreply.github.com>
2023-02-22 10:45:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ function compute()
     var amount = parseInt(principle) + parseFloat(interest);
     var result = document.getElementById("result");
     var year = new Date().getFullYear() + parseInt(years);
-    if (principal <= 0) {
+    
+    if (parseInt(principle) <= 0) {
         alert("Please enter a positive number!");
         document.getElementById("principal").focus();
-    } else {
+    } 
+    else {
         result.innerHTML = "If you deposit $" + "<mark>" + principle + "</mark>" + ",\<br\>at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\>You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\>in the year " + "<mark>" + year + "</mark>" + "\<br\>";
     }
 }
 
  function updateRate()
  {
-     var rateval = document.getElementById('rate').value;
+     var rateval = document.getElementById("rate").value;
      document.getElementById("rate_val").innerText = rateval;
 
  }
